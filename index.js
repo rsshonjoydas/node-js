dc3d9eba702619ed500ac538586ba84132b3ec9d
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-30 18:01:22 +0600

    Setup routing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const port = 5000
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('Hello World ' + req.query.id)
+})
+
 app.listen(port, () => {
   console.log("Listening on port" + port);
 })
