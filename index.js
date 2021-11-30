29bef68ffa3acea4b69d5ffefd95be1df536dea7
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-30 19:06:12 +0600

    Post request handling

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ import express from 'express';
 const port = 5000
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World ' + req.query.id)
+})
+
+app.post('/', (req, res) => {
+  // const body = JSON.stringify(req.body)
+  // res.send('Hello World for post request' + body)
+  const body = req.body
+  res.send('Hello World for post request ' + body.message)
 })
 
 app.listen(port, () => {
