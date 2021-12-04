35109963198fc50cfd3e81e258a337fa6a7f8ae0
rsshonjoydas <rsshonjoydas@gmail.com>
2021-12-04 09:26:06 +0600

    Refactor to split app and server

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import expressWinston from 'express-winston';
-import winston from 'winston';
-import 'winston-daily-rotate-file';
-import { ElasticsearchTransport } from 'winston-elasticsearch';
-import 'winston-mongodb';
 import configure from './controllers';
+import { errorLogger, infoLogger } from './logger';
 import { handleError } from './middlewares/handleError';
 import { processRequest } from './middlewares/processRequest';
-import { connectWithDB, options, uri } from './mongodb';
+import { connectWithDB } from './mongodb';
 
 // TODO: Express JS Configuration
 const app = express()
@@ -17,77 +13,6 @@ app.use(express.json())
 // TODO: dotenv Configuration
 dotenv.config()
 
-const getMessage = (req, res) => {
-  let obj = {
-    correlationId: req.headers['x-correlation-id'],
-    requestBody: req.body
-  }
-  return JSON.stringify(obj)
-}
-
-const fileInfoTransport = new (winston.transports.DailyRotateFile)(
-  {
-    filename: 'log-info-%DATE%.log',
-    datePattern: 'YYYY-MM-DD-HH',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d'
-  }
-);
-
-const fileErrorTransport = new (winston.transports.DailyRotateFile)(
-  {
-    filename: 'log-error-%DATE%.log',
-    datePattern: 'yyyy-MM-DD-HH',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d'
-  }
-);
-
-const mongoErrorTransport = new (winston.transports.MongoDB)({
-  db: uri,
-  metaKey: 'meta',
-  options,
-})
-
-const elasticsearchOptions = {
-  level: 'info',
-  clientOpts: { node: 'http://localhost:9200' },
-  indexPrefix: 'log-elasticsearch'
-}
-
-const esTransport = new (ElasticsearchTransport)(elasticsearchOptions)
-
-const infoLogger = expressWinston.logger({
-  transports: [
-    new winston.transports.Console(),
-    fileInfoTransport,
-    esTransport
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
-  meta: true,
-  // msg: 'This is a {{req.method}} request from {{req.url}}'
-  msg: getMessage
-});
-
-const errorLogger = expressWinston.errorLogger({
-  transports: [
-    new winston.transports.Console(),
-    fileErrorTransport,
-    mongoErrorTransport,
-    esTransport
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
-  meta: true,
-  msg: '{ "correlationId": "{{req.headers["x-correlation-id"]}}", "error": "{{err.message}}" }'
-});
 
 // TODO: Logger Configuration
 app.use(infoLogger)
---
+++ logger.js
@@ -0,0 +1,78 @@
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
+import 'winston-mongodb';
+import { options, uri } from './mongodb';
+
+const getMessage = (req, res) => {
+  let obj = {
+    correlationId: req.headers['x-correlation-id'],
+    requestBody: req.body
+  }
+  return JSON.stringify(obj)
+}
+
+const fileInfoTransport = new (winston.transports.DailyRotateFile)(
+  {
+    filename: 'log-info-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+  }
+);
+
+const fileErrorTransport = new (winston.transports.DailyRotateFile)(
+  {
+    filename: 'log-error-%DATE%.log',
+    datePattern: 'yyyy-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+  }
+);
+
+const mongoErrorTransport = new (winston.transports.MongoDB)({
+  db: uri,
+  metaKey: 'meta',
+  options,
+})
+
+const elasticsearchOptions = {
+  level: 'info',
+  clientOpts: { node: 'http://localhost:9200' },
+  indexPrefix: 'log-elasticsearch'
+}
+
+const esTransport = new (ElasticsearchTransport)(elasticsearchOptions)
+
+export const infoLogger = expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+    fileInfoTransport,
+    esTransport
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true,
+  // msg: 'This is a {{req.method}} request from {{req.url}}'
+  msg: getMessage
+});
+
+export const errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console(),
+    fileErrorTransport,
+    mongoErrorTransport,
+    esTransport
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true,
+  msg: '{ "correlationId": "{{req.headers["x-correlation-id"]}}", "error": "{{err.message}}" }'
+});
