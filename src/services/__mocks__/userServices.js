ba607486c93f3d8894703b7884ceb0856346a166
rsshonjoydas <rsshonjoydas@gmail.com>
2021-12-04 14:41:15 +0600

    Mock userService.getAllUsers method

---
+++ src/services/__mocks__/userServices.js
@@ -0,0 +1,7 @@
+
+export const getAllUsers = async (user) => {
+  const users = [{
+    '_id': '1', 'username': 'Shonjoy'
+  }]
+  return users
+}
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import app from '../src/app';
 
+jest.mock('../src/services/userServices')
+
 describe('app test suite', () => {
   test('should my first test suite', () => {
     console.log('my first test suite');
@@ -10,6 +12,7 @@ describe('app test suite', () => {
     console.log('app first test suite');
     let response = await request(app).get('/users');
     expect(response.statusCode).toBe(200);
-    console.log(response.body);
+    let users = response.body;
+    expect(users.length).toBe(1);
   })
 })
