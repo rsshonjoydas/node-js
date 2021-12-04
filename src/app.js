6a2de0d837f88d98a6ebca90b484db7b8825a2b9
rsshonjoydas <rsshonjoydas@gmail.com>
2021-12-04 18:51:40 +0600

    upgrade app server

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import configure from './controllers';
-import { errorLogger, infoLogger } from './logger';
 import { handleError, processRequest } from './middlewares';
-import { connectWithDB, options, uri } from './mongodb';
 
 // TODO: Express JS Configuration
 const app = express()
@@ -12,15 +10,6 @@ app.use(express.json())
 // TODO: dotenv Configuration
 dotenv.config()
 
-console.log(process.env.ENVIRONMENT);
-
-// TODO: Logger Configuration
-if (process.env.ENVIRONMENT !== 'development')
-  app.use(infoLogger)
-
-// TODO: Database Connection
-connectWithDB()
-
 // TODO: Correlation Id
 app.use(processRequest)
 
@@ -28,9 +17,6 @@ app.use(processRequest)
 configure(app)
 
 // TODO: Error Handler
-if (process.env.ENVIRONMENT !== 'development')
-  app.use(errorLogger(uri, options))
-
 app.use(handleError);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import app from './app';
+import { errorLogger, infoLogger } from './logger';
+import { connectWithDB, options, uri } from './mongodb';
 
 // TODO: App Configuration
 const APP_PORT = process.env.APP_PORT || 5000
 
 app.listen(APP_PORT, () => {
+  // TODO: Info Logger Configuration
+  if (process.env.ENVIRONMENT !== 'development')
+    app.use(infoLogger)
+
+  // TODO: Database Connection
+  connectWithDB()
+
+  // TODO: Error Logger Configuration
+  if (process.env.ENVIRONMENT !== 'development')
+    app.use(errorLogger(uri, options))
+
   console.log(`Listening on port ${APP_PORT}`);
 })
