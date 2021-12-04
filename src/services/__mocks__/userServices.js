a795e74c644c16c68d86fb84cd84cb937ee6ed84
rsshonjoydas <rsshonjoydas@gmail.com>
2021-12-04 21:18:46 +0600

    User controller Post request test

--- src/services/__mocks__/userServices.js
+++ src/services/__mocks__/userServices.js
@@ -1,7 +1,16 @@
+import models from "../../models";
+
+let users = [{
+  '_id': '1', 'username': 'Shonjoy'
+}]
 
 export const getAllUsers = async (user) => {
-  const users = [{
-    '_id': '1', 'username': 'Shonjoy'
-  }]
+
   return users
+}
+
+export const saveUser = async (user) => {
+  const userModel = new models.User(user);
+  users.push(userModel)
+  return userModel;
 }
--- tests/userController.test.js
+++ tests/userController.test.js
@@ -11,5 +11,13 @@ describe('userController test suite', () => {
     expect(users.length).toBeGreaterThan(0);
     expect(users[0]._id).toBe('1');
   })
+
+  test.only('post should return saved id', async () => {
+    let user = { username: 'test37' };
+    let response = await request(app).post('/users').send(user);
+    expect(response.statusCode).toBe(201);
+    let body = response.body;
+    expect(body.length).toBe(24);
+  })
 })
 
