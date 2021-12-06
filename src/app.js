d43462d4f988e725c1c5b600ca797d100ae3a6bc
rsshonjoydas <rsshonjoydas@gmail.com>
2021-12-06 23:06:51 +0600

    Necessary Packages

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import config from './config';
 import { errorLogger, infoLogger } from './logger';
@@ -7,9 +12,23 @@ import { options, uri } from './mongodb';
 import routes from './routes';
 import swaggerDocument from './swagger.json';
 
+// TODO: express-rate-limit options
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000 // limit each IP to 1000 requests per windowMs
+});
+
 // TODO: Express JS Configuration
 const app = express()
 app.use(express.json())
+
+// TODO: Necessary Packages
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(compression())
+app.use(cors())
+app.use(helmet())
+app.use(limiter)
 
 // TODO: Info Logger Configuration
 if (config.APP_ENVIRONMENT !== 'development')
